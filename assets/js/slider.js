a00317bce592f7463e032beb9845dc760f2948bc
unknown <mfaqihuddin23@gmail.com>
2022-05-17 14:39:59 +0700

    contact us

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -4,20 +4,20 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
     breakpoints: {
       // when window width is >= 320px
-      // 320: {
-      //   slidesPerView: 2,
-      //   spaceBetween: 20
-      // },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
       // when window width is >= 480px
-      // 480: {
-      //   slidesPerView: 3,
-      //   spaceBetween: 30
-      // },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
       // when window width is >= 640px
-      // 640: {
-      //   slidesPerView: 4,
-      //   spaceBetween: 40
-      // }
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
     },
     autoplay: {
       delay: 2000,

9ac166f5f43e502d01db7cf35ecf89dad29cd7a4
unknown <mfaqihuddin23@gmail.com>
2022-06-02 15:35:25 +0700

    index mobile done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -41,3 +41,30 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+const swiperlogo = new Swiper('.swiperlogo', {
+  // Optional parameters
+  
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    }
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  direction: 'horizontal',
+  loop: true,
+
+});

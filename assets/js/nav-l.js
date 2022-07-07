f89d716433e3270eb0e8c738724d0d5b5fd912e7
unknown <mfaqihuddin23@gmail.com>
2022-07-08 04:53:55 +0700

    revisi paytriot navscroll

--- assets/js/nav-l.js
+++ assets/js/nav-l.js
@@ -21,21 +21,3 @@ window.addEventListener('scroll', ()=>{
         }
     })
 })
-
-
-// window.onscroll = () => {
-//   var current = "";
-
-//   sections.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     if (window.pageYOffset >= sectionTop - 60) {
-//       current = section.getAttribute("id"); }
-//   });
-
-//   navLi.forEach((li) => {
-//     li.classList.remove("active");
-//     if (li.classList.contains(current)) {
-//       li.classList.add("active");
-//     }
-//   });
-// };

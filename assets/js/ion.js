2c1596e43b04dab0150f23d2b1198f1124c65300
unknown <mfaqihuddin23@gmail.com>
2022-09-22 14:48:53 +0700

    slider

--- assets/js/ion.js
+++ assets/js/ion.js
@@ -1,41 +1,45 @@
 var $range = $("#example_1");
-    var $input = $("#example_1_input");
-    var $input2 = $("#example_2_input");
-    var $input3 = $("#example_3_input");
-    var instance;
-    var min = 0;
-    var max = 1000;
+var $input = $("#example_1_input");
+var instance;
+var min = 0;
+var max = 1000;
+
+var $plan = $("#pricingplan")
+var es = document.getElementById("pricingplan");
+var val =es.value;
     
 $range.ionRangeSlider(
   {
     skin: "round",
-        type: "single",
-        min: min,
-        max: max,
-        from: 500,
-        onStart: function(data) {
-            $input.prop("value", data.from);
-        },
-        onChange: function(data) {
-            $input.prop("value", data.from);
-        }
+    type: "single",
+    min: min,
+    max: max,
+    from: val,
+    onStart: function(data) {
+        $input.prop("value", data.from);
+    },
+    onChange: function(data) {
+        $input.prop("value", data.from);
+    },
+    hide_min_max: true,
   }
 );
 
 instance = $range.data("ionRangeSlider");
 
+function checks(){
+  val =es.value;
+}
+$plan.on("change", function() {
+  checks()
+  instance.update({
+      from: val,
+  });
+  // checks()
+  // console.log(val);
+});
 $input.on("click", function() {
   instance.update({
       from: 50,
-  });
-});
-$input2.on("click", function() {
-  instance.update({
-      from: 300,
-  });
-});
-$input3.on("click", function() {
-  instance.update({
-      from: 150,
   });
 });
